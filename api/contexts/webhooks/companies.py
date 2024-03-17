cc81552572ea9f7d6a0755cc4c3051fa83fe3060
Mike Shanahan <mashanahan456@gmail.com>
2024-03-17 11:33:50 -0400

    Updated payload view for email webhook

--- api/contexts/webhooks/companies.py
+++ api/contexts/webhooks/companies.py
@@ -8,22 +8,19 @@ from api.vendors import db
 WEBHOOK_REQUEST_SCOPE = RequestScope(user_id="companies_webhook", db=db, company_id=None)
 
 
-async def verify_webhook_event(request: Request) -> dict:
-    return await request.json()
-
-
 router = APIRouter(
     tags=["Webhooks"],
     prefix="/webhooks/companies",
-    dependencies=[Depends(verify_webhook_event)],
 )
 
 
 @router.post("/jobs", status_code=status.HTTP_204_NO_CONTENT)
-async def jobs_webhook_handler(payload: dict, company_id: str):
-    print(payload)
+async def jobs_webhook_handler(request: Request):
+    print(await request.json())
+    return {"status": "ok"}
 
 
 @router.post("/applicants", status_code=status.HTTP_204_NO_CONTENT)
-async def applicants_webhook_handler(payload: dict, company_id: str):
-    print(payload)
+async def applicants_webhook_handler(request: Request):
+    print(await request.json())
+    return {"status": "ok"}
