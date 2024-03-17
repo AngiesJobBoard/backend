0e6245caf340a9d444c92e7e8847b55592f5a7b1
Mike Shanahan <mashanahan456@gmail.com>
2024-03-17 11:40:44 -0400

    More correct email reading

--- api/contexts/webhooks/companies.py
+++ api/contexts/webhooks/companies.py
@@ -1,7 +1,5 @@
-from fastapi import APIRouter, Request, status, Depends
-
+from fastapi import APIRouter, Request, status, File, Form, UploadFile
 from ajb.base import RequestScope
-
 from api.vendors import db
 
 
@@ -15,12 +13,36 @@ router = APIRouter(
 
 
 @router.post("/jobs", status_code=status.HTTP_204_NO_CONTENT)
-async def jobs_webhook_handler(request: Request):
-    print(await request.json())
-    return {"status": "ok"}
+async def jobs_webhook_handler(
+    from_email: str = Form(...),
+    subject: str = Form(...),
+    files: list[UploadFile] = File(default=None)
+):
+    print(f"Email from: {from_email}")
+    print(f"Subject: {subject}")
+
+    for file in files:
+        contents = await file.read()
+        # with open(file_location, "wb") as f:
+            # f.write(contents)
+        print(f"Saved file: {file.filename}")
+
+    return {"message": "Email received successfully"}
 
 
 @router.post("/applicants", status_code=status.HTTP_204_NO_CONTENT)
-async def applicants_webhook_handler(request: Request):
-    print(await request.json())
-    return {"status": "ok"}
+async def applicants_webhook_handler(
+    from_email: str = Form(...),
+    subject: str = Form(...),
+    files: list[UploadFile] = File(default=None)
+):
+    print(f"Email from: {from_email}")
+    print(f"Subject: {subject}")
+
+    for file in files:
+        contents = await file.read()
+        # with open(file_location, "wb") as f:
+            # f.write(contents)
+        print(f"Saved file: {file.filename}")
+
+    return {"message": "Email received successfully"}
