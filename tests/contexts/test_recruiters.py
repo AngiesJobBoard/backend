25f5d5f3922af159b9ab6487d247710ea1a01241
Jake Roman <jakemroman@gmail.com>
2024-07-02 13:15:48 -0400

    Added a test case for recruiter updates usecase

--- tests/contexts/test_recruiters.py
+++ tests/contexts/test_recruiters.py
@@ -1,3 +1,11 @@
+from ajb.contexts.applications.models import CreateApplication
+from ajb.contexts.applications.recruiter_updates.models import UpdateType
+from ajb.contexts.applications.recruiter_updates.usecase import RecruiterUpdatesUseCase
+from ajb.contexts.applications.repository import ApplicationRepository
+from ajb.contexts.applications.usecase import ApplicationUseCase
+from ajb.contexts.companies.jobs.models import UserCreateJob
+from ajb.contexts.companies.jobs.usecase import JobsUseCase
+from ajb.contexts.companies.models import ApplicationStatusRepresents
 from ajb.contexts.companies.recruiters.repository import (
     RecruiterRepository,
     CreateRecruiter,
@@ -113,3 +121,86 @@ def test_search_recruiters(request_scope):
 
     assert count == 1
     assert len(results) == 1
+
+
+def test_recruiter_updates(request_scope, mock_openai):
+    # Create recruiter
+    usecase = RecruiterUpdatesUseCase(request_scope)
+    user = UserFixture(request_scope).create_user()
+    company = CompanyFixture(request_scope).create_company()
+
+    repo = RecruiterRepository(request_scope, company.id)
+    created_recruiter = repo.create(
+        CreateRecruiter(
+            company_id=company.id, user_id=user.id, role=RecruiterRole.OWNER
+        )
+    )
+
+    # Create job & application
+    job_to_create = UserCreateJob(position_title="test")
+    job = JobsUseCase(request_scope, mock_openai).create_job(company.id, job_to_create)
+
+    application_usecase = ApplicationUseCase(request_scope)
+    application = application_usecase.create_application(
+        company.id,
+        job.id,
+        CreateApplication(
+            company_id=company.id,
+            job_id=job.id,
+            name="apply guy",
+            email="apply@guy.com",
+        ),
+        False,
+    )
+
+    # Add comment
+    usecase.add_recruiter_comment(
+        company.id,
+        job.id,
+        application.id,
+        user.id,
+        "Apply guy is a great fit for this position.",
+    )
+
+    timeline = usecase.get_application_update_timeline(
+        company.id, job.id, application.id
+    )[0]
+    assert (
+        len(timeline) == 1
+    )  # We should have one timeline entry from the add recruiter comment call
+    assert timeline[0].type == UpdateType.NOTE
+    assert timeline[0].comment == "Apply guy is a great fit for this position."
+
+    # Update application status
+    usecase.update_application_status(
+        company.id,
+        job.id,
+        application.id,
+        user.id,
+        ApplicationStatusRepresents.INTERESTED,
+        "I'd like to move forward to interview Apply guy",
+    )
+
+    timeline = usecase.get_application_update_timeline(
+        company.id, job.id, application.id
+    )[0]
+    assert (
+        len(timeline) == 2
+    )  # We should have another entry in the timeline from changing the status
+
+    # Find the new timeline entry (just in case the order in which they are returned is not deterministic)
+    update_status_entry = 0
+    if timeline[0].type == UpdateType.NOTE:
+        # We are looking for the update status entry, so use the other one
+        update_status_entry = 1
+
+    # Check for update application status entry
+    assert timeline[update_status_entry].type == UpdateType.STATUS_CHANGE
+    assert (
+        timeline[update_status_entry].new_application_status
+        == ApplicationStatusRepresents.INTERESTED
+    )
+    assert (
+        timeline[update_status_entry].comment
+        == "I'd like to move forward to interview Apply guy"
+    )
