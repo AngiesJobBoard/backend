9c3b0667174e4952bc1804613451022c1be60591
Jake Roman <jakemroman@gmail.com>
2024-07-09 12:27:48 -0400

    Added assertions to admin company test

--- tests/contexts/test_admin_company.py
+++ tests/contexts/test_admin_company.py
@@ -1,14 +1,19 @@
 from datetime import datetime
 from ajb.contexts.admin.companies.usecase import AdminCompanyUseCase
 from ajb.contexts.billing.billing_models import SubscriptionPlan
-from ajb.contexts.billing.subscriptions.models import CompanySubscription, CreateCompanySubscription, SubscriptionStatus
+from ajb.contexts.billing.subscriptions.models import (
+    CreateCompanySubscription,
+    SubscriptionStatus,
+)
+from ajb.contexts.billing.subscriptions.repository import CompanySubscriptionRepository
 from ajb.contexts.companies.models import CreateCompany
+from ajb.contexts.companies.repository import CompanyRepository
 
 
 def test_admin_create_company(request_scope):
     usecase = AdminCompanyUseCase(request_scope)
 
-    # Create company with subscription
+    # Prepare data
     company_object = CreateCompany(
         name="Test Company",
         slug="test",
@@ -24,4 +29,25 @@ def test_admin_create_company(request_scope):
         subscription_features=[],
         checkout_session=None,
     )
-    usecase.create_company_with_subscription(company_object, subscription_object)
+
+    # Create company with subscription
+    company = usecase.create_company_with_subscription(
+        company_object, subscription_object
+    )
+
+    # Validate company creation
+    company_repo = CompanyRepository(request_scope)
+    retrieved_company = company_repo.get(company.id)
+    assert retrieved_company.name == company_object.name
+    assert retrieved_company.owner_email == company_object.owner_email
+
+    # Validate subscription creation
+    company_subscription_repo = CompanySubscriptionRepository(request_scope, company.id)
+    subscription = company_subscription_repo.get_most_recent()
+
+    assert (
+        subscription.subscription_status == SubscriptionStatus.ACTIVE
+    )  # Ensure subscription is activated
+    assert (
+        subscription.plan == SubscriptionPlan.GOLD
+    )  # Check subscription plan is correct
